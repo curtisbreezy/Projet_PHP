57ef7f82c5f95d7e200172690f4c50021b68ef99
curtisbreezy <mourad.kheloui@gmail.com>
2018-07-13 12:16:23 +0200

    mise en forme du code plus propre

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if(!empty($_POST['nom']) AND!empty($_POST['email']) AND!empty($_POST['message'])
         $erreur = "Une erreur est survenue le mail n'est pas parti !";
         
         }
-    
+		header("Location : #contact");
     }
 
 ?>
@@ -31,9 +31,7 @@ if(!empty($_POST['nom']) AND!empty($_POST['email']) AND!empty($_POST['message'])
 
 <!DOCTYPE html>
 <html lang="fr">
-
   <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -48,13 +46,25 @@ if(!empty($_POST['nom']) AND!empty($_POST['email']) AND!empty($_POST['message'])
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
     <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
     <link href="css/creative.css" rel="stylesheet">
+	 <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+	<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+	<script>tinymce.init({ selector:'textarea' });</script>
   </head>
 
   <body id="page-top">
 
     <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top container-fluid" id="mainNav">
+	<nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top container-fluid" id="mainNav">
       <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="#page-top">Accueil</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -103,11 +113,10 @@ if(!empty($_POST['nom']) AND!empty($_POST['email']) AND!empty($_POST['message'])
             </h1>
             <hr>
           </div>
-          <div class="col-lg-8 mx-auto">
-            <p class="text-faded mb-5">Mourad Kheloui- Développeur PHP</p>
-            <a class="btn btn-primary btn-xl js-scroll-trigger" href="CV_2018_PHP.pdf">Plus d'informations</a>
-          </div>
-        
+			<div class="col-lg-8 mx-auto">
+				<p class="text-faded mb-5">Mourad Kheloui- Développeur PHP</p>
+				<a class="btn btn-primary btn-xl js-scroll-trigger" href="CV_2018_PHP.pdf">Plus d'informations</a>
+			</div>
       </div>
     </header>
 	
@@ -122,7 +131,6 @@ if(!empty($_POST['nom']) AND!empty($_POST['email']) AND!empty($_POST['message'])
             <p class="text-faded mb-4">Titulaire d'une licence gestion de la relation clientèle et e-commerce, j'ai durant cette troisième année, aborder les langages de développement web</p>
 			<p> Après plusieurs expériences dans le domaine commercial qui m'ont ennuyé, j'ai franchi le pas et est désormais étudiant en Master 1 développeur d'application PHP avec l'école de renommée 
 			internationale OpenClassrooms.</p>
-			
             <a class="btn btn-light btn-xl js-scroll-trigger" href="#contact">Rencontrons-nous !</a>
           </div>
         </div>
@@ -302,7 +310,7 @@ if(!empty($_POST['nom']) AND!empty($_POST['email']) AND!empty($_POST['message'])
           <div class="col-lg-4 mr-auto text-center">
             <i class="fa fa-envelope-o fa-3x mb-3 sr-contact"></i>
             <p>
-              <a href="mailto:your-email@your-domain.com">mourad.kheloui@gmail.com</a>
+              <a style="color:black;" href="mailto:your-email@your-domain.com">mourad.kheloui@gmail.com</a>
             </p>
           </div>
         </div>
@@ -311,64 +319,38 @@ if(!empty($_POST['nom']) AND!empty($_POST['email']) AND!empty($_POST['message'])
 	  
 	  <!-- ajout d'un formulaire de contact --->
 		
-			<div class="col-lg-8 col-md-12 col-xs-12 mx-auto text-center">
+			<div class="col-lg-8 col-md-8 col-xs-10 mx-auto text-center">
 				<form method="post" action="">
 					<div class="form-group">
+					
 					<?php
 					if(isset($erreur)){ echo"$erreur"; }
 					?>
 					
-					<h2 class="mt-4"> Formulaire de contact </h2>
-					<hr/>
-					<label for="nom">Vos coordonnées</label>
-					
-					<br/><input type="text" class="col-md-5 col-xs-6" name="nom" id="nom" placeholder="" required/>  <br/>
-					
-					<br/><label for="email">Votre email</label>
-					
-					<br/><input type="email" class="col-md-6" name="email" id="email" placeholder="" required/>
-					
-					<small id="emailHelp" class="form-text text-muted">Nous ne partagerons jamais vos données avec un tiers.</small>
-					
+						<h2 class="mt-4"> Formulaire de contact </h2> <hr/>
+							<label for="nom">Vos coordonnées</label> <br/>
+								<input type="text" class="col-md-5 col-xs-6" name="nom" id="nom" placeholder="" required/>  <br/>
+									<br/><label for="email">Votre email</label> <br/>
+								<input type="email" class="col-md-6" name="email" id="email" placeholder="" required/><br/>
+							<small id="emailHelp" class="form-text text-muted">Nous ne partagerons jamais vos données avec un tiers.</small>
+						<textarea class="col-xs-8 form-control" name="message" id="message" type="text" placeholder="Votre message"></textarea>	<hr/>
+					  <input type="submit" class="btn btn-success" value="Envoyer"></input>
 					</div>
-			
-					
-					
-					<textarea class="col-xs-12 form-control" name="message" id="message" type="text" placeholder="Votre message"></textarea>
-			 <hr/>
-			
-			
-			<input type="submit" class="btn btn-success" value="Envoyer"></input>
-				
 				</form>
-		
 			</div>
-		
-		
-		<!--------------------------------------fin------------------------------------------------>
     </section>
-	
+<!--------------------------------------fin------------------------------------------------>	
 	<footer class="sticky-footer text-center col-md-12 bg-dark">
        <div class="container">
 			<a class="btn btn-dark btn-xl sr-button col-md-6 mb-3 text-center" href="admin/admin.php">Espace abonné</a>
-	   <div>
-			<small>Copyright © Blog PHP-Mourad Kheloui-2018</small>
-	   </div>
-	  </div>
+				<div>
+					<small>Copyright © Blog PHP-Mourad Kheloui-2018</small>
+				</div>
+			  <div>
+			<a class="btn btn-dark btn-xl sr-button col-md-6 mt-3 text-center" href="mod/validatecomment.php">Espace modérateur</a>
+		</div>
+	</div>
       
     </footer>
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
-    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
-
-    <!-- Custom scripts for this template -->
-    <script src="js/creative.min.js"></script>
-	
   </body>
-
 </html>
